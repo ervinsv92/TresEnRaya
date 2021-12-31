7fc749d13cdb6e66ce241c5e25addfa5e1ab9bb0
Ervin <ervinsv92@gmail.com>
2021-12-30 19:46:07 -0600

    prueba

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
-const PORT = 3000;
+const PORT = 8080;
 const Socket = require('./utils/sockets');
 
 app.use(express.static(__dirname + '/public'));
